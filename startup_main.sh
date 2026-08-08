98751bbce7c7ae8199d2239d1d2f4954cfef75f6
Seturaman Kumar <seturamankumar@gmail.com>
2026-08-08 09:10:15 +0530

    Added node tainting to stop nginx pods from landing on the servers and added --flanel-face

--- startup_main.sh
+++ startup_main.sh
@@ -2,6 +2,8 @@
 apt update && apt upgrade -y
 apt install openssh-server -y
 systemctl enable --now ssh
-curl -sfL https://get.k3s.io | sh -s - server --cluster-init --tls-san 192.168.56.11 --node-ip 192.168.56.11
-sleep 120
+curl -sfL https://get.k3s.io | sh -s - server --cluster-init --tls-san 192.168.56.11 --node-ip 192.168.56.11 --flannel-iface enp0s8
+sleep 20
 sudo cat /var/lib/rancher/k3s/server/node-token >/vagrant/node-token
+cd /vagrant/
+sudo kubectl taint nodes k3s-server-1 CriticalAddonsOnly=true:NoExecute
--- startup_master.sh
+++ startup_master.sh
@@ -3,6 +3,8 @@ apt update && apt upgrade -y
 apt install openssh-server -y
 systemctl enable --now ssh
 IP="$1"
+NAME="$2"
 TOKEN=$(cat /vagrant/node-token)
-curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" sh -s - server --server https://192.168.56.11:6443 --tls-san "$IP" --node-ip "$IP"
-sleep 120
+curl -sfL https://get.k3s.io | K3S_TOKEN="$TOKEN" sh -s - server --server https://192.168.56.11:6443 --tls-san "$IP" --node-ip "$IP" --flannel-iface enp0s8
+sleep 20
+sudo kubectl taint nodes "$NAME" CriticalAddonsOnly=true:NoExecute
