2937001bc0a424b76795381efc662d5444e50e10
Seturaman Kumar <seturamankumar@gmail.com>
2026-08-08 09:13:54 +0530

    Agents configuration bash scrupt

--- startup_agent.sh
+++ startup_agent.sh
@@ -4,5 +4,5 @@ apt install openssh-server -y
 systemctl enable --now ssh
 IP="$1"
 TOKEN=$(cat /vagrant/node-token)
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.11:6443" K3S_TOKEN="$TOKEN" sh -s - agent --node-ip "$IP"
-sleep 120
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.11:6443" K3S_TOKEN="$TOKEN" sh -s - agent --node-ip "$IP" --flannel-iface enp0s8
+sleep 20
